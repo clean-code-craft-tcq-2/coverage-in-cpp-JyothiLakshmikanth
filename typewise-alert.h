31e30460f74bd9d83fd2e77dc3da12a3a87c7901
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-02-22 18:51:35 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -1,8 +1,8 @@
 #pragma once
 #ifndef TYPEWISEALERT_H
 #define TYPEWISEALERT_H
-#include <map>
-#include <vector>
+#include <iostream>
+#include <stdio.h>
 #include "TypeWiseAlerterUtility.h"
 
 using namespace std;
