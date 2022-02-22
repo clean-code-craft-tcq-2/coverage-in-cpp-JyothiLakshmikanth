32e68c6b42fef024a3c6c2066110b9780e9aec46
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-02-22 22:00:31 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -4,13 +4,29 @@
 #include "typewise-alert.h"
 
 using namespace InfoTypes;
+TypeWiseAlert typeWiseAlert;
 
 TEST_CASE("infers the breach according to limits") {
-  TypeWiseAlert typeWiseAlert;
+
   Limits low_limits(20, 30);
   REQUIRE(typeWiseAlert.inferBreach(12, low_limits) == TOO_LOW);
   Limits high_limits(20, 45);
   REQUIRE(typeWiseAlert.inferBreach(50, high_limits) == TOO_HIGH);
   Limits normal_limits(20, 45);
   REQUIRE(typeWiseAlert.inferBreach(30, normal_limits) == NORMAL);
+}
+
+TEST_CASE("checkAndAlert")
+{
+  BatteryCharacter batteryChar;
+  batteryChar.coolingType = PASSIVE_COOLING;
+  typeWiseAlert.checkAndAlert(TO_CONTROLLER, batteryChar, 45);
+}
+
+TEST_CASE("getTheLimitsForCoolingType")
+{
+  Limits limits;
+  limits = typeWiseAlert.getTheLimitsForCoolingType(PASSIVE_COOLING);
+  REQUIRE(limits.getLowerLimit() == 0.0);
+  REQUIRE(limits.getUpperLimit() == 35.0);
 }
