9eba1dc7490e54313b14272bf95f7cf18305987f
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-02-22 22:42:04 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -1,12 +1,12 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "test/catch.hpp"
-#include "gmock/gmock.h" 
+ 
 #define private public
 #include "typewise-alert.h"
 
 using namespace InfoTypes;
 TypeWiseAlert typeWiseAlert;
-void mock_sendToController(
+
 TEST_CASE("infers the breach according to limits") {
 
   Limits low_limits(20, 30);
