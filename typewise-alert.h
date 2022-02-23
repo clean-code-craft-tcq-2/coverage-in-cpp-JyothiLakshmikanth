7934c8eff53f2b04c9e5315cb7f3dfd6f41fe07b
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-02-23 20:20:18 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -30,10 +30,10 @@ public:
 private:
   BreachType inferBreach(double value,Limits limits);
   BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
-  void prepareCoolingTypeLimitsMap(T_CoolingTypeLimits& coolingTypeLinits);
-  void prepareBreachTypeStringMap(T_BreachTypeStringMap& breachTypeStringMap);
-  void prepareEmailRecepientList(EmailRecepientList& emailRecepientList);
-  void prepareControllerList(ControllerList& controllerList);
+  void prepareCoolingTypeLimitsMap();
+  void prepareBreachTypeStringMap();
+  void prepareEmailRecepientList();
+  void prepareControllerList();
   void sendToController(BreachType breachType);
   void sendToEmail(BreachType breachType);
   void updateAlerter(AlertTarget alertTarget, BreachType breachType);
