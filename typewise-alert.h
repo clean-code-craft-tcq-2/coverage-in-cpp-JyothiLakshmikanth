8213e0d953cd5c03c2e6608a124f2a3cb44db4cf
JyothiLakshmikanth <91040173+JyothiLakshmikanth@users.noreply.github.com>
2022-07-27 18:17:46 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -55,4 +55,70 @@ private:
     T_AlerterMap             m_alerterMap;
 };
 
+/* #include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+void updateStr(::std::string& up_str)
+{
+    std::cout <<"before - "<<up_str.c_str() << std::endl;
+    up_str = "efg";
+    std::cout <<"after - "<<up_str.c_str() << std::endl;
+}
+
+void withPointer()
+{
+    const char *sz = "hello";
+    std::cout << sizeof(sz) << std::endl;
+    std::cout <<sz[0] << std::endl;
+    ::std::string str = "abc";
+    const char *updated = str.c_str();
+    std::cout <<updated << std::endl;
+    ::std::string up = updated;
+    updateStr((up));
+    std::cout <<up.c_str() << std::endl;
+}
+
+void withArray()
+{
+    const char sz[] = "hello";
+    std::cout << sizeof(sz) << std::endl;
+    std::cout <<sz[0] << std::endl;
+}
+int main() {
+withPointer();
+::std::vector<string> a = {"hello", "hi","hallo","hey"};
+int key = 2;
+if(key>0)
+{
+    a[key].clear();
+}
+for(int i = 0; i<a.size(); i++)
+{
+    cout<<i<<" - "<<a[i]<<endl;
+}
+
+::std::map<char, string> str_map;
+str_map['a'] = "hello";
+str_map['b'] = "Hi";
+str_map['c'] = "Hey";
+str_map['d'] = "Hallo";
+
+char k = 'c';
+if(k!=0)
+{
+    str_map[k].clear();
+}
+::std::map<char, string>::const_iterator str_mapIt = str_map.begin();
+for(; str_mapIt != str_map.end(); str_mapIt++)
+{
+    cout<<str_mapIt->first<<" - "<<str_mapIt->second<<endl;
+}
+
+	return 0;
+}*/
+
 #endif
